4577f0d67e0cb491c350a60c59790932cd7f3787
joaomelara <3262.343@edu.fb.org.br>
2024-05-13 21:53:35 -0300

    we walkin slowly, but forward

--- MySQLBanco/banco.py
+++ MySQLBanco/banco.py
@@ -1,267 +1,131 @@
-#CLASSES
+# fase 1 - conexão
+import pymysql
+import os
+import time
 
+conexao = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='fbradesco',
+    database='bank'
+   )
 
-class Conta:
-    def __init__(self, numero, saldo, ativo, cpf):
-        self.numero = numero
-        self.saldo = saldo
-        self.ativo = ativo
-        self.cpf = cpf
-    
-    def credito(self, valor):
-        self.saldo += valor
-    
-    def debito(self, valor):
-        if self.saldo >= valor:
-            self.saldo -= valor
-        else:
-            print("Saldo insuficiente")
+os.system("cls")
 
-    def ativar(self):
-        if not self.ativo:
-            self.ativo = True
-        else:
-            print("Conta já está ativa")
+with conexao:
+    with conexao.cursor() as cursor:
+        TABLE_NAME = 'Poupanca'
+        #criação de uma tabela
+        cursor.execute(
+            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
+            'id INT AUTO_INCREMENT PRIMARY KEY , '
+            'numero INT NOT NULL,'
+            'cpf VARCHAR(20) NOT NULL, '
+            'saldo DOUBLE(10,2) NOT NULL,'
+            'ativa bool ,'
+            'diaAniversarioPoupanca INT NOT NULL'
+            ')'
+        )
+    conexao.commit()
+    with conexao.cursor() as cursor:
+        TABLE_NAME = 'Corrente'
+        #criação de uma tabela
+        cursor.execute(
+            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
+            'id INT AUTO_INCREMENT PRIMARY KEY , '
+            'numero INT NOT NULL,'
+            'cpf VARCHAR(11) NOT NULL, '
+            'saldo DOUBLE(10,2) NOT NULL,'
+            'ativa bool ,'
+            'contadorTalao INT NOT NULL'
+            ')'
+        )
+    conexao.commit()
+    with conexao.cursor() as cursor:
+        TABLE_NAME = 'Especial'
+        #criação de uma tabela
+        cursor.execute(
+            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
+            'id INT AUTO_INCREMENT PRIMARY KEY , '
+            'numero INT NOT NULL,'
+            'cpf VARCHAR(11) NOT NULL, '
+            'saldo DOUBLE(10,2) NOT NULL,'
+            'ativa bool ,'
+            'limite double(10,2) NOT NULL'
+            ')'
+        )
+    conexao.commit()    
 
-    def mostrar(self):
-        print(f"O seu saldo atual é {self.saldo}\n\n\n")
+    #MONTAGEM DO MENU DE ACESSO
+    while True:
+        os.system("cls")
+        print('-'*80)
+        print("BANK")
+        print('-'*80)
+        print("1 - Poupança")
+        print("2 - Corrente")
+        print("3 - Especial")
+        print("4 - Sair")
+        opcao_menu_principal=input("Digite sua opção :")
+        if opcao_menu_principal=='4':
+            break
+        elif opcao_menu_principal=='1':
+            while True:
+               
+                print('-'*80)
+                print("BANK - CONTA POUPANÇA")
+                print('-'*80)
+                print("1 - Cadastrar")
+                print("2 - Alterar")
+                print("3 - Apagar")
+                print("4 - Movimentar")
+                print("5 - Sair")
+                sub_opcao_menu=input("Digite sua opção :")
+                if sub_opcao_menu == '5':
+                    break
+                elif sub_opcao_menu =='1':
+                    with conexao.cursor() as cursor:
+                        TABLE_NAME = "poupanca"
+                        
+                        #inserindo dados em produto
+                        numero = int(input("Digite o numero da conta : "))
+                        cpf = input("Digita o cpf do cliente da conta :")
+                        dia_aniversario_conta = int(input("Digite o dia de aniversario da conta : "))
+                        dados = (numero, cpf, 0.00, 1, dia_aniversario_conta)
+                        sql = f'INSERT INTO {TABLE_NAME} (numero, cpf, saldo, ativa, diaAniversarioPoupanca) VALUES (%s,%s,%s,%s,%s)'
+                        cursor.execute(sql,dados)
+                        conexao.commit()
 
+                        sql =f'SELECT * FROM {TABLE_NAME}'
+                        cursor.execute(sql)
+                        resposta = cursor.fetchall()
+                        print("Dados cadastrados")                    
+                        for linha in resposta:
+                            print(linha)
+                    conexao.commit()
+                    time.sleep(3)
+                elif sub_opcao_menu == '4':
+                    with conexao.cursor() as cursor:
+                        TABLE_NAME = "poupanca"
+                        numero_conta = int(input("Digite o número da conta: "))
+                        
+                        # Verificar se a conta existe
+                        sql_verificar_conta = f'SELECT * FROM {TABLE_NAME} WHERE numero = %s'
+                        cursor.execute(sql_verificar_conta, (numero_conta,))
+                        conta = cursor.fetchone()
 
-class Poupanca(Conta):
-    def __init__(self, numero, saldo, ativo, cpf, aniversario):
-        super().__init__(numero, saldo, ativo, cpf)
-        self.aniversario = aniversario
-    
-    def atualizar_saldo(self, dia):
-        if self.aniversario == dia:
-            self.saldo += self.saldo * 0.05
-        else:
-            print("Sem atualização de saldo")
+                        if conta:
+                            print("Conta encontrada. Operações de movimentação podem ser realizadas.")
+                            for i in range(10):
+                                print("\n======= MENU =======")
+                                print("1. Débito")
+                                print("2. Crédito")
 
+                                op = int(input("Qual é a op?  "))
+                                if op == 1:
+                                    if conta[2] > 0:
+                                        valor = int(input("Valor?  "))
 
-class Corrente(Conta):
-    def __init__(self, numero, saldo, ativo, cpf):
-        super().__init__(numero, saldo, ativo, cpf)
-    
-    def pediTalao(self, cheques):
-        if 0 < cheques <= 3:
-            if self.saldo >= cheques * 30:
-                self.saldo -= cheques * 30
-            else: 
-                print("Saldo insuficiente para emitir talão de cheques")
-        else: 
-            print("Número inválido de cheques")
-
-
-class Especial(Conta):
-    def __init__(self, numero, saldo, ativo, cpf):
-        super().__init__(numero, saldo, ativo, cpf)
-        self.negativo_once = False
-
-    def debito(self, valor):
-        if self.saldo >= valor:
-            self.saldo -= valor
-        else:
-            if not self.negativo_once:
-                self.saldo -= valor
-                self.saldo += 1000  # Adiciona 1000 ao saldo
-                self.negativo_once = True
-                print("Saldo insuficiente. Limite ativado.")
-            else:
-                print("Saldo insuficiente. Limite já utilizado uma vez.")
-
-
-
-class Empresa(Conta):
-    def __init__(self, numero, saldo, ativo, cpf):
-        super().__init__(numero, saldo, ativo, cpf)
-        self.emprestimo = 10000
-    
-    def pedirEmprestimo(self, valor):
-        if valor <= self.emprestimo:
-            self.saldo += valor
-            self.emprestimo -= valor
-        else:
-            print("Valor de empréstimo excede o limite disponível")
-
-
-class Estudantil(Conta):
-    def __init__(self, numero, saldo, ativo, cpf):
-        super().__init__(numero, saldo, ativo, cpf)
-        self.emprestimo = 5000
-    
-    def pedirEmprestimoES(self, valor):
-        if valor <= self.emprestimo:
-            self.saldo += valor
-            self.emprestimo -= valor
-        else:
-            print("Valor de empréstimo excede o limite disponível")
-
-
-
-
-
-#APLICATIVO
-sair = False
-
-while sair == False:
-
-    tipo = input("Escolha o tipo de conta (Poupança(P), Corrente(C), Especial(S), Empresa(M), Estudantil(E)), Sair(Q): ")
-
-    if tipo.lower() == "p":
-        movimentos = 0
-        niver = int(input("Qual é o seu aniversário?\n"))
-        C1 = Poupanca(0, 0, True, None, niver)
-        while movimentos <= 3:
-            print("\n======= MENU =======")
-            print("1. Débito")
-            print("2. Crédito")
-            op = int(input("Qual é a op?  "))
-            if op == 1:
-                if C1.saldo > 0:
-                    valor = int(input("Valor?  "))
-                    C1.debito(valor)
-                    C1.mostrar()
-                    movimentos +=1
-                elif C1.saldo == 0:
-                    print("Deposite primeiro, sem saldo")
-                    C1.mostrar()
-            elif op == 2:
-                valor = int(input("Valor?  "))
-                C1.credito(valor)
-                C1.mostrar()
-                movimentos +=1
-            else:
-                print("Opção inválida")
-        data = int(input("Qual é o dia de hoje?"))
-        C1.atualizar_saldo(data)
-        C1.mostrar()
-
-    elif tipo.lower() == "c":
-        movimentos = 0
-        C1 = Corrente(0, 0, True, None)
-        while movimentos <= 3:
-            print("\n======= MENU =======")
-            print("1. Débito")
-            print("2. Crédito")
-            op = int(input("Qual é a op?  "))
-            if op == 1:
-                if C1.saldo > 0:
-                    valor = int(input("Valor?  "))
-                    C1.debito(valor)
-                    C1.mostrar()
-                    movimentos +=1
-                elif C1.saldo == 0:
-                    print("Deposite primeiro, sem saldo")
-                    C1.mostrar()
-            elif op == 2:
-                valor = int(input("Valor?  "))
-                C1.credito(valor)
-                C1.mostrar()
-                movimentos +=1
-            else:
-                print("Opção inválida")
-        op = input("Deseja cheques?(S/N)  ")
-        if op.lower() == 's':
-            qtd = int(input("Quantos precisas querido?  "))
-            C1.pediTalao(qtd)
-            C1.mostrar()
-        else:
-            print("bye")
-
-    elif tipo.lower() == "s":
-        movimentos = 0
-        C1 = Especial(0, 0, True, None)
-        while movimentos <= 3:
-            print("\n======= MENU =======")
-            print("1. Débito")
-            print("2. Crédito")
-            op = int(input("Qual é a op?  "))
-            if op == 1:
-                
-                valor = int(input("Valor?  "))
-                C1.debito(valor)
-                C1.mostrar()
-                
-                movimentos +=1
-                
-            elif op == 2:
-                valor = int(input("Valor?  "))
-                C1.credito(valor)
-                C1.mostrar()
-                
-                movimentos +=1
-            else:
-                print("Opção inválida")
-
-    elif tipo.lower() == "m":
-        movimentos = 0
-        C1 = Empresa(0, 0, True, None)
-        while movimentos <= 3:
-            print("\n======= MENU =======")
-            print("1. Débito")
-            print("2. Crédito")
-            print("3. Empréstimo")
-            op = int(input("Qual é a op?  "))
-            if op == 1:
-                if C1.saldo > 0:
-                    valor = int(input("Valor?  "))
-                    C1.debito(valor)
-                    C1.mostrar()
-                    movimentos +=1
-                elif C1.saldo == 0:
-                    print("Deposite primeiro, sem saldo")
-                    C1.mostrar()
-            elif op == 2:
-                valor = int(input("Valor?  "))
-                C1.credito(valor)
-                C1.mostrar()
-                movimentos +=1
-            elif op == 3:
-                valor = int(input("Valor?  "))
-                C1.pedirEmprestimo(valor)
-                movimentos+=1
-                C1.mostrar()
-            else:
-                print("Opção inválida")
-        
-    elif tipo.lower() == "e":
-        movimentos = 0
-        C1 = Estudantil(0, 0, True, None)
-        while movimentos <= 3:
-            print("\n======= MENU =======")
-            print("1. Débito")
-            print("2. Crédito")
-            print("3. Empréstimo")
-            op = int(input("Qual é a op?  "))
-            if op == 1:
-                if C1.saldo > 0:
-                    valor = int(input("Valor?  "))
-                    C1.debito(valor)
-                    C1.mostrar()
-                    movimentos +=1
-                elif C1.saldo == 0:
-                    print("Deposite primeiro, sem saldo")
-                    C1.mostrar()
-            elif op == 2:
-                valor = int(input("Valor?  "))
-                C1.credito(valor)
-                C1.mostrar()
-                movimentos +=1
-            elif op == 3:
-                valor = int(input("Valor?  "))
-                C1.pedirEmprestimoES(valor)
-                movimentos+=1
-                C1.mostrar()
-            else:
-                print("Opção inválida")
-
-    elif tipo.lower() == "q":
-        print("Good bye mah lovah")
-        sair = True
-        break
-
-    else:
-        print("Tipo de conta inválido!")
-
+                        else:
+                            print("Conta não encontrada. Verifique o número da conta.")
 
